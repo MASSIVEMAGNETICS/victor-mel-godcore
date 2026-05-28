4fc8c14e8c9d9ef76359a213914db4bdc029a504
IAMBANDOBANDZ <massivemagnetics@gmail.com>
2026-05-28 00:48:10 -0400

    Add convenient top-level run_demo.py

---
+++ run_demo.py
@@ -0,0 +1,10 @@
+"""
+Top-level runnable demo for victor-mel-godcore.
+
+This is the easiest way for someone to experience the system after cloning.
+"""
+
+from examples.educational_mel_demo import main
+
+if __name__ == "__main__":
+    main()
